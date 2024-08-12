48e9084fc558f3f19a626c1085929c934cc41452
jimao <shihao.gu@perfma.com>
2024-08-12 13:43:46 +0800

    class

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 
 export default
 function Header() {
-  return <header className="fixed top-0 left-0 right-0 h-16 border-b">
+  return <header className="fixed top-0 left-0 right-0 h-16 border-b bg-white z-20">
     Header
   </header>;
 }
--- app/components/Sider.tsx
+++ app/components/Sider.tsx
@@ -1,6 +1,6 @@
 
 export default
 function Sider() {
-  return <div className="fixed top-16 left-0 bottom-0 w-52 border-r">
+  return <div className="fixed top-16 left-0 bottom-0 w-52 border-r bg-white z-20">
   </div>;
 }

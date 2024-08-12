79d2bafb2388613cd6921221a25ebca4f59eecbd
jimao <shihao.gu@perfma.com>
2024-08-12 13:41:38 +0800

    Sider

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "@remix-run/react";
 import Header from "./components/Header";
+import Sider from "./components/Sider";
 import Content from "./components/Content";
 
 export default
 function App() {
   return <>
     <Header />
+    <Sider />
     <Content />
     {/* <Outlet /> */}
   </>;
---
+++ app/components/Sider.tsx
@@ -0,0 +1,6 @@
+
+export default
+function Sider() {
+  return <div className="fixed top-16 left-0 bottom-0 w-52 border-r">
+  </div>;
+}

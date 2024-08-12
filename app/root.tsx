d179e85a869d4f03417dd10ad29b50ce1cae3190
jimao <shihao.gu@perfma.com>
2024-08-12 13:28:33 +0800

    你好,世界

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="m-0 p-0">
         {children}
         <ScrollRestoration />
         <Scripts />

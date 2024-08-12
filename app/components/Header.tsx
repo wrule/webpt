aaff50e67a63fc9c447f435b4deaece89faf4d49
jimao <shihao.gu@perfma.com>
2024-08-12 13:56:17 +0800

    header demo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 
 export default
 function Header() {
-  return <header className="fixed top-0 left-0 right-0 h-16 border-b bg-white z-20">
-    Header
+  return <header className="fixed top-0 left-0 right-0 h-16 border-b bg-white z-20 flex justify-between items-center px-4">
+    <h1 className="text-4xl font-medium">DEMO</h1>
   </header>;
 }
